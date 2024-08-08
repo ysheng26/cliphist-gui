cadc81f9c664a99e946bb7f265f1153fa4ee3cdc
Yun Sheng <uewing@gmail.com>
2024-08-07 22:37:10 -0400

    update

--- build-app.sh
+++ build-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./appify -author "Yun Sheng" -icon ./assets/icon-256.png -id "ysheng26.bingus.bungus" -name "cliphist" -version $1 ./target/release/cliphist_gui
+./appify -author "Yun Sheng" -icon ./assets/icon-256.png -id "ysheng26.bingus.bungus" -name "Cliphist" -version $1 ./target/release/cliphist_gui
