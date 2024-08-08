0da84d089aa0f36bd66ff35fbe09a2e7351bf5f3
Yun Sheng <uewing@gmail.com>
2024-08-07 22:38:37 -0400

    pull git tag as version

--- build-app.sh
+++ build-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./appify -author "Yun Sheng" -icon ./assets/icon-256.png -id "ysheng26.bingus.bungus" -name "Cliphist" -version $1 ./target/release/cliphist_gui
+./appify -author "Yun Sheng" -icon ./assets/icon-256.png -id "ysheng26.bingus.bungus" -name "Cliphist" -version $(git tag --points-at HEAD) ./target/release/cliphist_gui
